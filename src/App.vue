62d43b34c56706583e2cc5011d5ebb4714c6c7f2
Raúl López <hi@raullg.com>
2021-01-17 22:47:34 +0000

    Add loading page

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,27 @@
       <Notification title="Copied!" text="Command has successfully been copied to your clipboard." v-if="shouldDisplayNotification" />
     </transition>
     <Banner @copy="displayNotification" @analysis="analyse" />
+    <Results :results="results" :loading="loading" ref="results" />
   </div>
 </template>
 
 <script>
 import Banner from './components/Banner.vue'
 import Notification from './components/Notification.vue'
+import Results from './components/Results.vue'
 
 export default {
   name: 'app',
   components: {
     Banner,
-    Notification
+    Notification,
+    Results
   },
   data () {
     return {
-      shouldDisplayNotification: false
+      shouldDisplayNotification: false,
+      loading: false,
+      results: null
     }
   },
   methods: {
@@ -30,6 +35,16 @@ export default {
       }, 2000)
     },
     analyse (url, keyword) {
+      if (this.loading) {
+        return
+      }
+
+      this.results = null
+      this.loading = true
+      setTimeout(() => {
+        this.$refs.results.$el.scrollIntoView({ behavior: 'smooth' })
+      }, 200)
+
       fetch('https://litic.herokuapp.com/invoke', {
         method: 'POST',
         headers: {
@@ -43,10 +58,10 @@ export default {
       })
         .then(response => response.json())
         .then(json => {
-          console.log(json)
+          this.results = json.results
         })
         .catch(error => {
-          console.log(error)
+          // console.log(error)
         })
     }
   }
---
+++ src/components/Results.vue
@@ -0,0 +1,45 @@
+<template>
+  <div
+    v-if="loading"
+    class="h-screen flex justify-center items-center p-4 text-white text-4xl lg:text-6xl transition duration-500"
+    :class="bg"
+  >
+    Fetching data
+    <span v-for="dot in dots" :key="dot">.</span>
+  </div>
+  <div v-else>
+    {{results}}
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'Results',
+  props: {
+    results: {
+      type: Array,
+      required: false
+    },
+    loading: {
+      type: Boolean,
+      required: true
+    }
+  },
+  data () {
+    return {
+      bg: 'bg-gray-800',
+      dots: 0
+    }
+  },
+  mounted () {
+    setInterval(() => {
+      this.dots = (this.dots + 1)%4
+    }, 1000)
+
+    setInterval(() => {
+      this.bg = this.bg === 'bg-gray-700' ? 'bg-gray-800' : 'bg-gray-700'
+    }, 500)
+  }
+}
+</script>
